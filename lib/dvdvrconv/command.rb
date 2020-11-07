68e22dacdca38562259ae037f9681e23b51da944
icm7216 <icm7216d@gmail.com>
2020-11-07 19:49:13 +0900

    Add conversion process

--- lib/dvdvrconv/command.rb
+++ lib/dvdvrconv/command.rb
@@ -8,13 +8,28 @@ module Dvdvrconv
       @argv = argv
     end
 
-    # returm to dummy dvd-vr info for test command
+    # For now, this test code returns dummy dvd-vr information.
     def execute
-      # test command line
       dvd = Dvdvrconv::Dvdvr.new
-      dvd.dvdvr_opts = "test/DVD_RTAV//VR_MANGR.IFO"
+      # dvd.dvdvr_opts_ifo = "test/DVD_RTAV/VR_MANGR.IFO"
+      # dvd.dvdvr_opts_vro = "test/DVD_RTAV/VR_MOVIE.VRO"
       dvd.read_info
-      dvd.view_info
+      # dvd.view_info
+
+      # Extract vob files
+      title, dup_name = dvd.adjust_title
+      dvd.vro2vob
+
+      # customize title of vob files
+      vob_titles = dvd.customize_title(title)
+      dvd.rename_vob(vob_titles)
+
+      # Concatenate Split Titles
+      concat_list = dvd.make_concat_list(title, dup_name)
+      dvd.concat_titles(concat_list)
+
+      # convert vob to mp4
+      dvd.vob2mp4
     end
   end
 end
